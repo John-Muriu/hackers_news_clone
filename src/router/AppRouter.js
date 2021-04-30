2dbe16c3eaa8b38a3d75a42f032f083bde645e5f
John Muriu <johnmuriw@gmail.com>
2021-05-01 00:31:01 +0300

    showstories

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from '../components/Header';
-import HomePage from '../components/HomePage';
+import { Redirect } from 'react-router';
 import PageNotFound from '../components/PageNotFound';
+import ShowStories from '../components/ShowStories';
+
 
 const AppRouter = () => {
     return (
@@ -10,7 +12,17 @@ const AppRouter = () => {
             <div className="container">
                 <Header />
                 <Switch>
-                    <Route path="/" component={HomePage} exact={true} />
+                    <Route path="/" render={() => <Redirect to="/top" />} exact={true} />
+                    <Route
+                        path="/:type"
+                        render={({ match }) => {
+                            const { type } = match.params;
+                            if (!['top', 'new', 'best'].includes(type)) {
+                                return <Redirect to="/" />;
+                            }
+                            return <ShowStories type={type} />;
+                        }}
+                    />
                     <Route component={PageNotFound} />
                 </Switch>
             </div>
