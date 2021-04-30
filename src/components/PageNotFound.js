9a17c11f33407e5a2831969de470e8ce5d9d721c
John Muriu <johnmuriw@gmail.com>
2021-04-30 23:21:30 +0300

    added the pageNotFound file

---
+++ src/components/PageNotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+
+const PageNotFound = () => {
+    return (
+        <p>
+            Page Not Found. Go to <Link to='/'> Home </Link>
+        </p>
+    );
+
+};
+export default PageNotFound;
