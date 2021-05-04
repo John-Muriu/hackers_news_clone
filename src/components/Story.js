a5c75f1f364ba88f11ff00fbd5d3d14130b57898
John Muriu <johnmuriw@gmail.com>
2021-05-04 15:31:35 +0300

    dummy text

--- src/components/Story.js
+++ src/components/Story.js
@@ -1,24 +1,24 @@
 import React from 'react';
-// import { LoremIpsum } from 'react-lorem-ipsum';
+import { LoremIpsum } from "react-lorem-ipsum";
 
 const Link = ({ url, title }) => (
     <a href={url} target="_blank" rel="noreferrer">
         {title}
     </a>
 );
-https://www.openindiana.org/2021/05/01/openindiana-hipster-2021-04-is-here/
-
 const Story = ({ story: { id, by, title, kids, time, url } }) => {
     return (
         <div className="story">
             <div className="story-title">
                 <Link url={url} title={title} />
             </div>
-            <div className="story-description">
-                <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-                        e who loves pain itself, who seeks af</p>
-            </div>
 
+            <div className="story-description">
+                <LoremIpsum
+                    avgWordsPerSentence={4}
+                    avgSentencesPerParagraph={4}
+                />
+            </div>,
             <div className="story-info">
                 <span>
                     by{' '}
